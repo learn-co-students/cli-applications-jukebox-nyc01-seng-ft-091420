362a50e556ffa826397744be949289a092b28c09
Bryan Moon <bryanmoon1991@gmail.com>
2020-09-14 01:06:47 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -52,4 +52,8 @@ end
 
 
 
-#this lab really confused me because technically after you input list, help, or if play returns the invalid input message, run(songs) needs to occur again. Also for the play method, the test is first looking for the method to return the name of the song, but then fails when you dont return "playing <song name>".  remember to ask instructor about how this code was tested
+# note to self - remember to ask instructor !
+
+# this lab confused me because technically after you input list, help, or if play returns the invalid input message, run(songs) needs to occur again. Also for the play method, the last test on the spec is first looking for the method to return the name of the song, but then fails when you dont return "playing <song name>".  
+
+# also directions ask for the play method to be able to find a and input of the song name, not the <artist - song name> which is what the spec is testing
